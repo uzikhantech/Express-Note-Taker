d618b225b11dcb1a568fdacbb370f705617e1216
Khan <Usman.Khan@pega.com>
2020-06-12 09:37:37 -0400

    If no matching route is found default to home using the * character

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/notes", (req, res) => {
   });
 
 // If no matching route is found default to home
- app.get("/", (req, res) => {
+ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
